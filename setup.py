92ae08908014d7c20ae16ed2f70d99661789d526
Jeffrey <jeffrey.ray@bell.net>
2025-11-09 16:45:26 -0500

    update(setup)

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup, find_packages
 setup(
 	name='cardlib',
 	version='1.0.0',
-	packages=find_packages(),
-	description='Reusable card and dice library for text-based games',
 	author='Jeffrey Ray',
+	author_email='jeffrey.ray@bell.net',
+	description='Reusable card and dice library for text-based games',
+	long_description=open('README.md', encoding='utf-8').read(),
+	long_description_content_type='text/markdown',
 	url='https://github.com/jray-8/cardlib',
 	license='MIT',
+	packages=find_packages(),
 	python_requires='>=3.8',
 	classifiers=[
 		'Programming Language :: Python :: 3',
